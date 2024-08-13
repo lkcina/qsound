e1ccf4188d5e806e34b06b5275d43e371e36a220
Landon Cina <lkcina6@gmail.com>
2024-08-13 13:41:13 -0400

    Start app and library components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,16 +8,43 @@ class App extends React.Component {
       library: [],
       events: []
     };
+
+    this.editLibrary = this.editLibrary.bind(this);
+  }
+
+  editLibrary(array) {
+    this.setState({library: array});
   }
 
   render() {
     return (
       <div id="app">
         My React App!
+        <Library library={this.state.library}/>
       </div>
     );
   }
-
 };
+
+class Library extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addAudioFile = this.addAudioFile.bind(this);
+  }
+
+  addAudioFile() {
+  }
+
+  render() {
+    return (
+      <div id="library">
+        <h2>Library</h2>
+        <div class="toolbar">
+          <button >+ Audio File</button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
