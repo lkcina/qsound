e07691b752920fed9aba64ae8bc8db794a8375ea
Landon Cina <lkcina6@gmail.com>
2024-08-14 10:34:24 -0400

    Add client side add and delete audio file functions. Needs more work.

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
     return (
       <div id="app">
         My React App!
-        <Library library={this.state.library}/>
+        <Library library={this.state.library} editLibrary={this.editLibrary}/>
       </div>
     );
   }
@@ -44,10 +44,19 @@ class Library extends React.Component {
     super(props);
     this.addAudioFile = this.addAudioFile.bind(this);
     this.playAudioFile = this.playAudioFile.bind(this);
+    this.delAudioFile = this.delAudioFile.bind(this);
   }
 
   addAudioFile() {
     console.log("Adding audio file");
+    // User selects file from file dialog, which is uploaded to project audio library and added to the library state
+    
+  }
+
+  delAudioFile(event) {
+    console.log("Deleting audio file");
+    // User deletes audio file from library, which is removed from the project audio library and the library state
+    this.props.editLibrary(this.props.library.filter(audioFile => audioFile.id !== event.target.parentElement.querySelector("audio").id));
   }
 
   playAudioFile(event) {
@@ -76,7 +85,7 @@ class Library extends React.Component {
         <div className="audio-file-container">
           {this.props.library.map(audioFile => (
 
-            <AudioFile key={audioFile.id} id={audioFile.id} name={audioFile.name} src={audioFile.src} playAudioFile={this.playAudioFile}/>
+            <AudioFile key={audioFile.id} id={audioFile.id} name={audioFile.name} src={audioFile.src} playAudioFile={this.playAudioFile} delAudioFile={this.delAudioFile}/>
           ))}
         </div>          
       </div>
@@ -89,6 +98,7 @@ const AudioFile = (props) => {
     <div className="audio-file">
       <button className="library-play-btn" onClick={props.playAudioFile}>Play</button>
       <p>{props.name}</p>
+      <button className="library-del-btn" onClick={props.delAudioFile}>Delete</button>
       <audio id={props.id} src={props.src} type={props.type}></audio>
       
     </div>
