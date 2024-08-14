209e3db234f3eb4e53b7090ee3da93e51b65cfb0
Landon Cina <lkcina6@gmail.com>
2024-08-14 08:32:56 -0400

    fix audio file path bug

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      library: [],
+      library: [
+        {
+          id: "action-adventure-demo",
+          name: "Action_Adventure_Demo.wav",
+          src: "test-library/Action_Adventure_Demo.wav",
+          type: "audio/wav"
+        },
+        {
+          id: "theme-loop",
+          name: "Theme Loop.wav",
+          src: "test-library/Theme Loop.mp3",
+          type: "audio/mpeg"
+        }
+    ],
       events: []
     };
 
@@ -30,9 +43,15 @@ class Library extends React.Component {
   constructor(props) {
     super(props);
     this.addAudioFile = this.addAudioFile.bind(this);
+    this.playAudioFile = this.playAudioFile.bind(this);
   }
 
   addAudioFile() {
+
+  }
+
+  playAudioFile() {
+      
   }
 
   render() {
@@ -40,11 +59,27 @@ class Library extends React.Component {
       <div id="library">
         <h2>Library</h2>
         <div class="toolbar">
-          <button >+ Audio File</button>
+          <input type="file" />
         </div>
+        <div class="audio-file-container">
+          {this.props.library.map(audioFile => (
+            <AudioFile id={audioFile.id} name={audioFile.name} src={audioFile.src}/>
+          ))}
+        </div>          
       </div>
     );
   }
+}
+
+const AudioFile = (props) => {
+  return (
+    <div class="audio-file">
+      <p>{props.name}</p>
+      <audio controls>
+        <source src={props.src} type={props.type}/>
+      </audio>
+    </div>
+  );
 }
 
 export default App;
