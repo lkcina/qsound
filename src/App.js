08ff3fca02084f2df84e9438fc4c0a54c58aa5ea
Landon Cina <lkcina6@gmail.com>
2024-09-16 14:03:26 -0400

    Add Event delete button functionality. Also, transferred all event methods to EventTimeline.js and created a generic editEvents method in App

--- src/App.js
+++ src/App.js
@@ -58,10 +58,7 @@ class App extends React.Component {
     this.triggerEvent = this.triggerEvent.bind(this);
     this.setEventStart = this.setEventStart.bind(this);
     this.setActiveEvent = this.setActiveEvent.bind(this);
-    this.editEventName = this.editEventName.bind(this);
-    this.editEventNotes = this.editEventNotes.bind(this);
-    this.editEventId = this.editEventId.bind(this);
-    this.newEvent = this.newEvent.bind(this);
+    this.editEvents = this.editEvents.bind(this); 
   }
 
   editLibrary(array) {
@@ -102,47 +99,8 @@ class App extends React.Component {
     }
   };
 
-  editEventName(event) {
-    let newEvents = [...this.state.events];
-    newEvents[newEvents.findIndex(e => e.id === event.target.parentElement.id)].name = event.target.value;
-    this.setState({events: newEvents});
-  }
-
-  editEventNotes(event) {
-    let newEvents = [...this.state.events];
-    newEvents[newEvents.findIndex(e => e.id === event.target.parentElement.id)].notes = event.target.value;
-    this.setState({events: newEvents});
-  }
-
-  editEventId(event) {
-    let newEvents = [...this.state.events];
-    let newId = event.target.value.toLowerCase().replace(/\s/g, "-");
-    let idCopies = 0;
-    newEvents.map(e => {
-      if (e.id !== event.target.parentElement.id && e.id === newId) {
-        idCopies++;
-        newId = idCopies === 1 ? newId + "-" + idCopies : newId.slice(0, newId.length - 1) + idCopies;
-      };
-      return newId;
-    });
-    newEvents[newEvents.findIndex(e => e.id === event.target.parentElement.id)].id = newId;
-    this.setState({events: newEvents});
-  }
-
-  newEvent() {
-    let newEvents = [...this.state.events];
-    newEvents.splice(newEvents.length - 1, 0, {id: `event-${newEvents.length}`, name: `Event ${newEvents.length}`, notes: ""});
-    let newId = newEvents[newEvents.length - 2].id;
-    let idCopies = 0;
-    newEvents.map(e => {
-      if (e.id === newId && e !== newEvents[newEvents.length - 2]) {
-        idCopies++;
-        newId = idCopies === 1 ? newId + "-" + idCopies : newId.slice(0, newId.length - 1) + idCopies;
-      };
-      return newId;
-    });
-    newEvents[newEvents.length - 2].id = newId;
-    this.setState({events: newEvents});
+  editEvents(array) {
+    this.setState({events: array});
     console.log(this.state.events);
   }
 
@@ -150,7 +108,7 @@ class App extends React.Component {
     return (
       <div id="app">
         <Library library={this.state.library} editLibrary={this.editLibrary} />
-        <EventTimeline events={this.state.events} mode={this.state.mode} toggleMode={this.toggleMode} setEventStart={this.setEventStart} eventStart={this.state.eventStart} activeEvent={this.state.activeEvent} setActiveEvent={this.setActiveEvent} triggerEvent={this.triggerEvent} editEventName={this.editEventName} editEventNotes={this.editEventNotes} editEventId={this.editEventId} newEvent={this.newEvent}/>
+        <EventTimeline events={this.state.events} mode={this.state.mode} toggleMode={this.toggleMode} setEventStart={this.setEventStart} eventStart={this.state.eventStart} activeEvent={this.state.activeEvent} setActiveEvent={this.setActiveEvent} triggerEvent={this.triggerEvent} editEvents={this.editEvents} editEventId={this.editEventId} newEvent={this.newEvent} deleteEvent={this.deleteEvent}/>
       </div>
     );
   }
