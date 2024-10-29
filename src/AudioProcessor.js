dad8eefdf10827bbdf38538efe4443e1b558def6
Landon Cina <lkcina6@gmail.com>
2024-10-29 15:43:30 -0400

    Another attempt

--- src/AudioProcessor.js
+++ src/AudioProcessor.js
@@ -1,24 +1,47 @@
 import React from 'react';
 
 
+const audioContext = new AudioContext();
+const masterGain = audioContext.createGain();
+const output = audioContext.destination;
+masterGain.connect(output);
+
+masterGain.gain.value = 0.2;
 
 class AudioProcessor extends React.Component {
     constructor(props) {
         super(props);
+        
     }
 
     render() {
-        const audioContext = new AudioContext();
-        this.cues.forEach(cue => {
-            // Create audio node with gain node and route to destination
-            const buffer = audioContext.createBuffer();
-            const source = audioContext.createBufferSource();
-            const gainNode = audioContext.createGain();
-        });
+        
+        if (this.props.mode === "present") {
+            audioContext.resume();
+            this.props.cues.forEach(cue => {
+                const audioElement = document.getElementById(cue.id + "-audio");
+                const source = audioContext.createMediaElementSource(audioElement);
+                const gainNode = audioContext.createGain();
+                source.connect(gainNode);
+                gainNode.connect(masterGain);
+                console.log(audioElement, gainNode, source);
+            });
+            console.log(document.getElementById("cue-1-audio"));
+        }
 
         return (
             <div>
                 <h1>Audio Processor</h1>
+                {this.props.cues.map(cue => {
+                    
+                    
+                    return (
+                        <div key={cue.id}>
+                            <audio id={cue.id + "-audio"} src={cue.src}></audio>
+                            <button id={cue.id + "-button"} onClick={() => document.getElementById(cue.id + "-audio").play()}>{cue.name}</button>
+                        </div>
+                    )
+                })}
             </div>
         );
     }
